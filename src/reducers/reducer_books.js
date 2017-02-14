40b1f20f80f7d86c4a37d367272f415c4ff0af4b
Jcook894 <jcook894@icloud.com>
2017-02-14 01:37:55 -0500

    Feat: Added Book list reducer.

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+function(){
+  return[
+    {title: "A Series of Unfortunate Events"},
+    {title: "Harry Potter"},
+    {title: "The Dark Tower"},
+    {title: "Night"},
+    {title: "The Phantom TollBooth"},
+  ]
+}
