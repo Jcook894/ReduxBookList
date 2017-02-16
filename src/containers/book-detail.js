0e10566a67e9615724fd955b8ed0e75efcbea478
Jcook894 <jcook894@icloud.com>
2017-02-16 01:57:15 -0500

    Feat: Madethe Book detail a container component.

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
 
-export default class BookDetail extends Component{
+class BookDetail extends Component{
   render(){
       return(
         <div> Book Details!!! </div>
       );
   }
 };
+
+
+function mapStateToProps(state){
+  return {
+    book: activeBook
+  };
+}
+
+export default connect(mapStateToProps)(BookDetail);
