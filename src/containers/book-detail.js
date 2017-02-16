fad0cc6b6fe0882dee0abc3f087681123ea53dcb
Jcook894 <jcook894@icloud.com>
2017-02-16 00:41:59 -0500

    Feat: connected the bookdetail container to the app.

---
+++ src/containers/book-detail.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+export default class BookDetail extends Component{
+  render(){
+      return(
+        <div> Book Details!!! </div>
+      );
+  }
+};
